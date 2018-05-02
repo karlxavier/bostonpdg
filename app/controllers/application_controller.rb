36696d807cccc6bbb0fffa490e8ff3da77625e3b
Jerico Lopez <jerico@maddington.net>
2018-05-02 09:44:06 +0800

    RE-76: Implement add item on order view

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,4 +7,8 @@ class ApplicationController < ActionController::Base
   def configure_permitted_parameters
     devise_parameter_sanitizer.permit(:sign_up, keys: [:email,:password, :password_confirmation, roles: []])
   end
+
+  def params
+    request.parameters
+  end
 end
--- app/models/order_entry.rb
+++ app/models/order_entry.rb
@@ -3,4 +3,14 @@ class OrderEntry < ApplicationRecord
     product = Product.find(self.product_id)
     product.name
   end
+
+  def quoted_name
+    user = User.find(self.quoted_by)
+    user.first_name + " " + user.last_name
+  end
+
+  def vendor_name
+    vendor = Vendor.find(self.vendor)
+    vendor.name
+  end
 end
