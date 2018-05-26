f60064f453b03f4409757a4181ac0251aa89c2d1
Jerico Lopez <jerico@maddington.net>
2018-05-26 16:42:44 +0800

    RE-83 [PART 1]

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -6,4 +6,21 @@ class OrdersController < ApplicationController
     OrderMailer.with(order: @order, vendor: @vendor, order_entries: @order_entries).send_order_entries.deliver_now
     redirect_to admin_order_path(@order)
   end
+
+  def index
+    @orders = Order.order("updated_at, created_by DESC")
+    if @orders.present? && !@orders.nil?
+      if params[:id].present? && !params[:id].nil?
+        @order = Order.find(params[:id])
+        @order_entries = OrderEntry.where("order_id = '#{@order.id}'")
+        @order_users = OrderUser.where("order_id = '#{@order.id}'")
+      else
+        @order = Order.order('updated_at, created_by DESC').first
+        @order_entries = OrderEntry.where("order_id = '#{@order.id}'")
+        @order_users = OrderUser.where("order_id = '#{@order.id}'")
+      end
+
+    end
+  end
+
 end
