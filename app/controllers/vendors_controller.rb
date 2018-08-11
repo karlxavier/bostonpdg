92ea76776d9ec506156dec6b84a922fa10a3bf97
Jerico Lopez <jerico@maddington.net>
2018-08-11 11:58:12 +0800

    Vendors

--- app/controllers/vendors_controller.rb
+++ app/controllers/vendors_controller.rb
@@ -6,6 +6,25 @@ class VendorsController < ApplicationController
     redirect_to admin_vendors_path
   end
 
+  def create
+    @vendor = Vendor.new
+    @vendor.name = params[:name]
+    @vendor.email = params[:email]
+    @vendor.billing_address = params[:address]
+    @vendor.country_origin = params[:country]
+    if @vendor.save
+      if params[:category_id].present?
+        params[:category_id].each do |id|
+          VendorCategory.create(:vendor_id => @vendor.id, :category_id => id)
+        end
+      end
+      flash[:notice] = "Vendor has saved successfully."
+    else
+      flash[:error] = "Vendor save Failed!"
+    end
+    redirect_to inventories_path
+  end
+
   def new
 
   end
