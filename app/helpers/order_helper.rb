37701295ff975c0d96744c63a1825238914b1546
Joener Preagola <joener@maddington.net>
2018-08-16 08:05:15 +0800

    fixed heroku error

--- app/helpers/order_helper.rb
+++ app/helpers/order_helper.rb
@@ -1,2 +1,14 @@
 module OrderHelper
+
+	def check_image_url entry 
+		if entry.product.present?
+			if  entry.product.image_url.present?
+			   entry.product.image_url(:original)
+			else
+			  nil
+			end	
+		else
+			nil	
+		end	
+	end	
 end
