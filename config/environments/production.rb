36730d5a981adf9724d28789d8e7aa0463df2498
karlxavier <kxdc1025@yahoo.com>
2018-09-20 20:18:09 +0400

    change request origin name to *.aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,8 @@ Rails.application.configure do
   # config.action_cable.mount_path = nil
   # config.action_cable.url = 'wss://example.com/cable'
   # config.action_cable.allowed_request_origins = [ 'http://example.com', /http:\/\/example.*/ ]
-  config.action_cable.allowed_request_origins = [ /(http|https):\/\/.*\.cmgresearch\.net/ ]
+  # config.action_cable.allowed_request_origins = [ /(http|https):\/\/.*\.cmgresearch\.net/ ]
+  config.action_cable.allowed_request_origins = [ /(http|https):\/\/.*\.aws-production.5fsrj3kfbx.us-west-2.elasticbeanstalk\.com/ ]
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
