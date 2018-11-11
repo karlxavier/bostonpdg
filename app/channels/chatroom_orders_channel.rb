a567caa97996fbc651d7e4d81791fff3324df34c
karlxavier <kxdc1025@yahoo.com>
2018-11-11 22:56:55 +0400

    enable websockets for heroku

--- app/channels/chatroom_orders_channel.rb
+++ app/channels/chatroom_orders_channel.rb
@@ -1,17 +1,17 @@
 class ChatroomOrdersChannel < ApplicationCable::Channel
-  	# def subscribed
-  	# 	if current_user.admin?
-  	# 		ChatroomOrder.all.each do |chatroom|
-	#       		stream_from "chatrooms:#{chatroom.id}"
-	#     	end
-  	# 	else
-	#     	ChatroomOrder.broadcast_chatrooms(current_user.id).each do |chatroom|
-	#       		stream_from "chatrooms:#{chatroom.id}"
-	#     	end
-	#     end
-  	# end
+  	def subscribed
+  		if current_user.admin?
+  			ChatroomOrder.all.each do |chatroom|
+	      		stream_from "chatrooms:#{chatroom.id}"
+	    	end
+  		else
+	    	ChatroomOrder.broadcast_chatrooms(current_user.id).each do |chatroom|
+	      		stream_from "chatrooms:#{chatroom.id}"
+	    	end
+	    end
+  	end
 
-  	# def unsubscribed
-    # 	stop_all_streams
-  	# end
+  	def unsubscribed
+    	stop_all_streams
+  	end
 end
--- app/channels/item_messages_channel.rb
+++ app/channels/item_messages_channel.rb
@@ -1,12 +1,12 @@
 class ItemMessagesChannel < ApplicationCable::Channel
-  	# def subscribed
+  	def subscribed
 
-    #   Product.all.each do |product|
-    #     stream_from "items:#{product.id}"
-    #   end
-  	# end
+      Product.all.each do |product|
+        stream_from "items:#{product.id}"
+      end
+  	end
 
-  	# def unsubscribed
-    # 	stop_all_streams
-  	# end
+  	def unsubscribed
+    	stop_all_streams
+  	end
 end
