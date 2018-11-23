6f61d03a2ba0637abd659c76f8e5b28791d677ef
karlxavier <kxdc1025@yahoo.com>
2018-11-23 17:07:02 +0400

    remove migration add column channel_id in channel_users

--- db/migrate/20181122102907_add_column_channel_to_message.rb
+++ db/migrate/20181122102907_add_column_channel_to_message.rb
@@ -1,5 +1,5 @@
 class AddColumnChannelToMessage < ActiveRecord::Migration[5.1]
   def change
-    add_column :channel_users, :channel_id, :integer
+    # add_column :channel_users, :channel_id, :integer
   end
 end
