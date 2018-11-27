3835b54b61f03823c3f7e489779b5cd099957b8a
karlxavier <kxdc1025@yahoo.com>
2018-11-27 19:21:56 +0400

    enable channel migrations for adding new coluns

--- db/migrate/20181123131918_add_column_channel_id_to_message.rb
+++ db/migrate/20181123131918_add_column_channel_id_to_message.rb
@@ -1,5 +1,5 @@
 class AddColumnChannelIdToMessage < ActiveRecord::Migration[5.1]
   def change
-    # add_column :messages, :channel_id, :integer
+    add_column :messages, :channel_id, :integer
   end
 end
